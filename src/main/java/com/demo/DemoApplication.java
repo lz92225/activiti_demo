fa0bd5294af7d14a439fba5b06f3e6ac2b6565c3
lz92225 <lz4233>
2019-08-27 19:03:36 +0800

    activiti_demo1

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.demo;
 
+import org.activiti.api.runtime.shared.query.Page;
+import org.activiti.api.runtime.shared.query.Pageable;
+import org.activiti.api.task.model.Task;
 import org.activiti.api.task.model.builders.TaskPayloadBuilder;
 import org.activiti.api.task.runtime.TaskRuntime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +15,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
     @Autowired
     private TaskRuntime taskRuntime;
 
@@ -24,11 +31,40 @@ public class DemoApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         securityUtil.logInAs("salaboy");
 
+        logger.info("> Creating a Group Task for 'activitiTeam'");
         taskRuntime.create(TaskPayloadBuilder.create()
                 .withName("task test1")
                 .withDescription("description")
                 .withCandidateGroup("group")
                 .withPriority(10)
                 .build());
+
+        securityUtil.logInAs("other");
+
+        logger.info("> Getting all the tasks");
+        Page<Task> tasks = taskRuntime.tasks(Pageable.of(0, 10));
+
+        logger.info(">  other cannot see the task: " + tasks.getTotalItems());
+
+        securityUtil.logInAs("erdemedeiros");
+
+        // Let's get 'erdemedeiros' tasks
+        logger.info("> Getting all the tasks");
+        tasks = taskRuntime.tasks(Pageable.of(0, 10));
+
+        // 'erdemedeiros' can see and claim the task
+        logger.info(">  erdemedeiros can see the task: " + tasks.getTotalItems());
+
+
+        String availableTaskId = tasks.getContent().get(0).getId();
+
+        // Let's claim the task, after the claim, nobody else can see the task and 'erdemedeiros' becomes the assignee
+        logger.info("> Claiming the task");
+        taskRuntime.claim(TaskPayloadBuilder.claim().withTaskId(availableTaskId).build());
+
+
+        // Let's complete the task
+        logger.info("> Completing the task");
+        taskRuntime.complete(TaskPayloadBuilder.complete().withTaskId(availableTaskId).build());
     }
 }
